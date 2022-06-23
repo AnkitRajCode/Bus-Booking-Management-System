78ef1dd8c007907f4fd347c7c4fa5430fa8a11ab
AnkitRajCode <rajankit432@gmail.com>
2022-06-23 16:56:55 +0530

    Footer setup

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='px-20'>
+    <div className='px-20 h-screen w-full bg-cover bg-center' style={{backgroundImage:'url(/Images/footer.svg)'}} >
         Footer
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,12 @@ module.exports = {
         roboto:['Roboto', 'sans-serif'],
       },
       backgroundImage: {
-        'bodybackground' : 'linear-gradient(294.47deg, #130B2B 20.94%, #2E1371 100%);',
+        'bodybackground' : 'linear-gradient(294.47deg, #130B2B 20.94%, #2E1371 100%)',
+        'navbarButton' : 'linear-gradient(91.38deg, #FB4A4A -10.93%, #2027CC 99.61%)',
+
+      },
+      boxShadow: {
+        'nav': '0px 10px 40px rgba(186, 64, 113, 0.35)',
       },
     },
   },
