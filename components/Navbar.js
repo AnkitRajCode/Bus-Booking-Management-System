dadbdfa0693a71636ced1d0ca0a6f6cc13cd288a
satyajeetramnit <1906130@kiit.ac.in>
2022-06-23 16:51:57 +0530

    Nav Bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,17 +3,23 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center font-semibold py-6'>
-        <Image src="/Images/bhanu.svg" alt="logo" width="160px" height="100px" />
-        <div className='flex space-x-6 items-center'>
+    <div className='flex justify-between items-center font-semibold'>
+        <Image src="/Images/bhanu.svg" alt="logo" width="120px" height="80px" />
+        <div className='flex space-x-10 items-center'>
             <Link href='/'>
-                <a className=''>Home</a>
+                <a><i className="fa-solid fa-house mr-2"></i><span>Home</span></a>
             </Link>
-            <Link href='/profile'>
-                <a className=''>Profile</a>
+            <Link href='#aboutus'>
+                <a><i className="fa-solid fa-location-dot mr-2"></i><span>Location</span></a>
+            </Link>
+            <Link href='#aboutus'>
+                <a><i className="fa-brands fa-readme mr-2"></i><span>About Us</span></a>
+            </Link>
+            <Link href='/booking'>
+                <a><i className="fa-solid fa-swatchbook mr-2"></i><span>Booking</span></a>
             </Link>
             <Link href='/user'>
-                <a className='px-4 hover:shadow-md text-sm py-1 bg-green-600 rounded-sm'>Signup</a>
+                <a className='px-4 hover:shadow-nav transition-all text-md py-1 bg-navbarButton rounded-sm'><i className="fa-solid fa-arrow-right-to-bracket"></i> Signup</a>
             </Link>
         </div>
     </div>
