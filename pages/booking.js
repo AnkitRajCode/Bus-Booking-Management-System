3ca71f76b417044cd269f974e194b7ed26070633
AnkitRajCode <rajankit432@gmail.com>
2022-06-30 02:55:33 +0530

    booking desgin done in sys file

--- pages/booking.js
+++ pages/booking.js
@@ -1,34 +1,95 @@
-// import React, { useState } from 'react';
-// import Seat from '../components/Seat';
-
-// const Booking = () => {
-
-//     const [seats, setSeats] = useState([]);
-
-
-//   return (
-//     <div>
-//         <SeatContext.Provider value={{seats, setSeats}}>
-//             <Seat  />
-//             {seats.length>0 && seats.map((seat, index) => {
-//             // return (
-//             //   <div key={index}>{seat}</div>
-//             // )
-//           })}
-//         </SeatContext.Provider>
-//     </div>
-//   )
-// }
-
-// export default Booking
-
-import React from 'react'
-
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 const booking = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='mx-20 mb-20'>
+        <Navbar />
+        <div className='flex justify-center'>
+          <div className='w-[330px] h-[700px] bg-contain bg-center bg-no-repeat' style={{backgroundImage:"url(/Images/booking.svg)"}}>
+            <div className='ml-[75px] pt-52'>
+              <i className="fa-solid fa-square"></i>
+              <span className='ml-[140px]'>
+                <i className="fa-solid fa-square"></i>
+                <i className="fa-solid fa-square ml-7"></i>
+              </span>
+            </div>
+            <div className='ml-[75px] pt-7'>
+              <i className="fa-solid fa-square"></i>
+              <span className='ml-[140px]'>
+                <i className="fa-solid fa-square"></i>
+                <i className="fa-solid fa-square ml-7"></i>
+              </span>
+            </div>
+            <div className='ml-[75px] pt-7'>
+              <i className="fa-solid fa-square"></i>
+              <span className='ml-[140px]'>
+                <i className="fa-solid fa-square"></i>
+                <i className="fa-solid fa-square ml-7"></i>
+              </span>
+            </div>
+            <div className='ml-[75px] pt-7'>
+              <i className="fa-solid fa-square"></i>
+              <span className='ml-[140px]'>
+                <i className="fa-solid fa-square"></i>
+                <i className="fa-solid fa-square ml-7"></i>
+              </span>
+            </div>
+            <div className='ml-[75px] pt-7'>
+              <i className="fa-solid fa-square"></i>
+              <span className='ml-[140px]'>
+                <i className="fa-solid fa-square"></i>
+                <i className="fa-solid fa-square ml-7"></i>
+              </span>
+            </div>
+            <div className='ml-[75px] pt-7'>
+              <i className="fa-solid fa-square"></i>
+              <span className='ml-[140px]'>
+                <i className="fa-solid fa-square"></i>
+                <i className="fa-solid fa-square ml-7"></i>
+              </span>
+            </div>
+            <div className='ml-[75px] pt-7'>
+              <i className="fa-solid fa-square"></i>
+              <span className='ml-[140px]'>
+                <i className="fa-solid fa-square"></i>
+                <i className="fa-solid fa-square ml-7"></i>
+              </span>
+            </div>
+            <div className='ml-[75px] pt-7'>
+              <i className="fa-solid fa-square"></i>
+              <span className='ml-[140px]'>
+                <i className="fa-solid fa-square"></i>
+                <i className="fa-solid fa-square ml-7"></i>
+              </span>
+            </div>
+            <div className='ml-[75px] pt-7'>
+              <i className="fa-solid fa-square"></i>
+              <span className='ml-[140px]'>
+                <i className="fa-solid fa-square"></i>
+                <i className="fa-solid fa-square ml-7"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className='py-5 flex justify-evenly'>
+          <div>
+            <div className='text-xl font-semibold'>Seat</div>
+            <div>A1</div>
+          </div>
+          <div>
+            <div className='text-xl font-semibold'>Price</div>
+            <div>$ 45</div>
+          </div>
+        </div>
+        <div className='flex justify-center'>
+          <div className='py-1 px-4 bg-green-700 rounded-md'>Checkout</div>
+        </div>
+
+      </div>
+      <Footer />
+    </>
   )
 }
 
